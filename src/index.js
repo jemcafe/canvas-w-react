dee8e757004dd1cf295874ee4c034cfa07d887e8
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-05-01 01:49:48 -0500

    Initial commit. Created canvas and a point can be drawn.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles/index.css';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Canvas from './components/Canvas';
+
+ReactDOM.render(<Canvas />, document.getElementById('root'));
